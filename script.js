6f137a2c73c8cc955b8d40cb394e12d21f43f494
mahmud07-bgmi <hussainmahmud919@gmail.com>
2026-04-20 18:32:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.12
+});
+
+reveals.forEach((item) => observer.observe(item));
